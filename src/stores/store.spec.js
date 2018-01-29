1216b2bd55837c515713d4107db71f10c3ccbf44
aherlihy <anna.herlihy@10gen.com>
2018-01-29 16:47:44 +0200

    balancerEnabled test

--- src/stores/store.spec.js
+++ src/stores/store.spec.js
@@ -1,4 +1,3 @@
-import expect from 'chai';
 import store from 'stores';
 import AppRegistry from 'hadron-app-registry';
 import Connection from 'mongodb-connection-model';
@@ -15,18 +14,18 @@ describe('ClusterHealthStore [Store]', () => {
   const dataService = new DataService(CONNECTION);
   const appRegistry = new AppRegistry();
   
-  before((done) => {
-    appRegistry.registerStore('ClusterHealth.Store', store);
-    store.onActivated(appRegistry);
-    dataService.connect(() => {
-      appRegistry.emit('data-service-connected', null, dataService);
-      done();
-    });
-  });
-  
-  after(() => {
-    dataService.disconnect();
-  });
+  // before((done) => {
+  //   appRegistry.registerStore('ClusterHealth.Store', store);
+  //   store.onActivated(appRegistry);
+  //   dataService.connect(() => {
+  //     appRegistry.emit('data-service-connected', null, dataService);
+  //     done();
+  //   });
+  // });
+  //
+  // after(() => {
+  //   dataService.disconnect();
+  // });
   
   describe('mockup', () => {
     beforeEach(() => {
@@ -35,21 +34,55 @@ describe('ClusterHealthStore [Store]', () => {
     });
   
     it('runs a test', () => {
-      console.log(store.state);
+      // console.log(store.state);
     })
   });
   
-  describe('with dataservice', () => {
-    before(() => {
-      store.mockup = false;
-    });
-    it('triggers correctly for balancer enabled', (done) => {
-      const unsubscribe = store.listen(() => {
-        unsubscribe();
-        console.log(store.state);
-        done();
+  describe('#balancerEnabled', () => {
+    describe('when dataservice returns no documents', () => {
+      before(() => {
+        store.dataService = {
+          find: (ns, filter, opts, callback) => {
+            callback(null, []);
+          }
+        }
       });
-      store.loadData();
+      it('sets balancerEnabled to true', (done) => {
+        store.getBalancerEnabled(null, () => {
+          expect(store.data.balancerEnabled).to.equal(true);
+          done();
+        });
+      });
+    });
+    describe('when dataservice returns documents without stopped', () => {
+      before(() => {
+        store.dataService = {
+          find: (ns, filter, opts, callback) => {
+            callback(null, [{}]);
+          }
+        }
+      });
+      it('sets balancerEnabled to true', (done) => {
+        store.getBalancerEnabled(null, () => {
+          expect(store.data.balancerEnabled).to.equal(true);
+          done();
+        });
+      });
+    });
+    describe('when dataservice returns documents with stopped documents', () => {
+      before(() => {
+        store.dataService = {
+          find: (ns, filter, opts, callback) => {
+            callback(null, [{stopped: true}]);
+          }
+        }
+      });
+      it('sets balancerEnabled to false', (done) => {
+        store.getBalancerEnabled(null, () => {
+          expect(store.data.balancerEnabled).to.equal(false);
+          done();
+        });
+      });
     });
   });
 });
